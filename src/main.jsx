cc4c015e2e64fbfcacefd0fee5c92b23d7a4bf25
VizzDum <attorney.kucher@gmail.com>
2025-05-18 21:21:22 +0300

    Add AOS animation library

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// AOS (Animate On Scroll)
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-AOS.init({
-  duration: 800,
-  once: true,
-  easing: 'ease-in-out',
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
